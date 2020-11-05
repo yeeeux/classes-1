e85c764a5918bc8bded27990dc92572da7f81480
goldaxe10 <you@example.com>
2020-11-05 17:33:50 +0500

    del

--- Ford.py
+++
@@ -1,4 +0,0 @@
-from car import Car as Car
-class Ford(Car):
-    def __init__(self, carName, maxSpeed):
-        super().__init__(carName, maxSpeed)
--- Toyota.py
+++
@@ -1,3 +0,0 @@
-class Toyota(Car):
-    def __init__(self, carName, maxSpeed):
-        super().__init__(carName, maxSpeed)
